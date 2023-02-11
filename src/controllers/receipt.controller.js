44929481b87e213bb6155e2d4d89f3ee345cb249
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-02-11 15:30:34 -0500

    Update receipt controller
    Add methods to edit and delete receipts

--- src/controllers/receipt.controller.js
+++ src/controllers/receipt.controller.js
@@ -92,4 +92,86 @@ receiptCtrl.getReceipt = async (req, res) => {
   }
 }
 
+receiptCtrl.updateReceipt = async (req, res) => {
+  try {
+    const _id = req.params.id
+    const createdBy = req.user
+    const { name, description, currency, type, address, createdAt } = req.body
+    const receiptQuery = await receiptSchema.findOne({
+      _id,
+      createdBy
+    })
+    if (!receiptQuery) return responseHandler(404)(res)
+
+    if (description && (!description.title || !description.price)) {
+      return responseHandler(
+        400,
+        "'title' and 'price' fields in 'description' are required"
+      )(res)
+    }
+
+    if (type && (type === TYPES.invoice)) {
+      const { billTo } = req.body
+      if (!billTo || !billTo.name || !billTo.email) {
+        return responseHandler(
+          400,
+          "'name' and 'email' fields in 'billTo' are required"
+        )(res)
+      }
+
+      await receiptSchema.findByIdAndUpdate(_id, {
+        name,
+        type,
+        description,
+        currency,
+        address,
+        createdAt,
+        billTo
+      })
+    } else if (type && (type === TYPES.ticket)) {
+      await receiptSchema.findByIdAndUpdate(_id, {
+        name,
+        type,
+        description,
+        currency,
+        address,
+        createdAt,
+        billTo: []
+      })
+    } else if (!type) {
+      await receiptSchema.findByIdAndUpdate(_id, {
+        name,
+        description,
+        currency,
+        address,
+        createdAt
+      })
+    } else {
+      return responseHandler(400, 'Receipt type error')(res)
+    }
+
+    return responseHandler(200, 'Receipt updated successfully')(res)
+  } catch (error) {
+    return responseHandler(400)(res)
+  }
+}
+
+receiptCtrl.deleteReceipt = async (req, res) => {
+  try {
+    const _id = req.params.id
+    const createdBy = req.user
+    const receiptQuery = await receiptSchema.findOne({
+      _id,
+      createdBy
+    })
+    if (!receiptQuery) return responseHandler(404)(res)
+
+    await receiptSchema.findByIdAndRemove(_id)
+
+    return responseHandler(200, 'Receipt deleted successfully')(res)
+  } catch (error) {
+    return responseHandler(400)(res)
+  }
+}
+
 module.exports = receiptCtrl
