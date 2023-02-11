3acaad89549e76186257d6396e9bf8eedd126b6e
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-02-11 15:31:45 -0500

    Update receipt routes
    Add routes to edit and delete receipts

--- src/routes/receipt.route.js
+++ src/routes/receipt.route.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const router = Router()
-const { createReceipt, getAllReceipt, getReceipt } = require('../controllers/receipt.controller')
+const { createReceipt, getAllReceipt, getReceipt, updateReceipt, deleteReceipt } = require('../controllers/receipt.controller')
 const verifyToken = require('../middlewares/verifyToken')
 
 router.post('/create', verifyToken, createReceipt)
 router.get('/all', verifyToken, getAllReceipt)
 router.get('/get/:id', verifyToken, getReceipt)
+router.put('/update/:id', verifyToken, updateReceipt)
+router.delete('/delete/:id', verifyToken, deleteReceipt)
 
 module.exports = router
