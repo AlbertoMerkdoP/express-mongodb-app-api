21b42863522634337296fab143be5dc7e30f742d
AlbertoMerkdoP <jacksoon2004@gmail.com>
2023-02-11 13:58:10 -0500

    Update routes
    Add middleware for access token verification

--- src/routes/receipt.route.js
+++ src/routes/receipt.route.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 const { createReceipt, getAllReceipt, getReceipt } = require('../controllers/receipt.controller')
+const verifyToken = require('../middlewares/verifyToken')
 
-router.post('/create', createReceipt)
-router.get('/all/:createdBy', getAllReceipt)
-router.get('/get/:id', getReceipt)
+router.post('/create', verifyToken, createReceipt)
+router.get('/all', verifyToken, getAllReceipt)
+router.get('/get/:id', verifyToken, getReceipt)
 
 module.exports = router
